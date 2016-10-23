ef4a3eb604d1cb788facf8177ccedcb4933255fc
yskoverride <yskoverride@gmail.com>
2016-10-23 18:04:33 +0530

    Bit of cleanup

--- src/routes.php
+++ src/routes.php
@@ -1,9 +1,3 @@
 <?php
 
 $router->get('','PagesController@home');
-$router->get('contact','PagesController@contact');
-$router->get('ranger','PagesController@ranger');
-$router->post('contact','PagesController@contactus');
-
-$router->post('profile','PagesController@profileupload');
-$router->get('profile','PagesController@profile');
